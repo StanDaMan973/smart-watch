db35aae811d0159413a0e2d44305a3767b4b6aa3
StanDaMan973 <stanley.espaillat@gmail.com>
2020-03-15 10:12:23 -0400

    Onscroll function is working. active class changes with the section

--- js/smartwatch.js
+++ js/smartwatch.js
@@ -1,4 +1,3 @@
-
 // intersection Observer
 
 const header = document.querySelector('header');
@@ -9,17 +8,40 @@ var sectionOneOptions = {
     rootMargin: "-200px 0px 0px 0px"
 };
 
-const coverSectionObserver = new IntersectionObserver(function(entries, coverSectionObserver) {
+const coverSectionObserver = new IntersectionObserver(function (entries, coverSectionObserver) {
 
     entries.forEach(entry => {
-        if(!entry.isIntersecting) {
-            header.classList.add('nav-scrolled'); 
+        if (!entry.isIntersecting) {
+            header.classList.add('nav-scrolled');
         } else {
-            header.classList.remove('nav-scrolled'); 
+            header.classList.remove('nav-scrolled');
         }
     })
 
-}, sectionOneOptions); 
-
+}, sectionOneOptions);
 
 coverSectionObserver.observe(sectionOne);
+
+
+// onScroll 
+
+function onScroll(event) {
+    let navLinks = document.querySelectorAll('#navbar a');
+    let scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+    
+    for (let i = 0; i < navLinks.length; i++) {
+        var currLink = navLinks[i];
+        var val = currLink.getAttribute('href');
+        var refElement = document.querySelector(val);
+        if(refElement.offsetTop <= scrollPos && (refElement.offsetTop + refElement.offsetHeight > scrollPos)) {
+           document.querySelector('#navbar ul li a').classList.remove('link-color');
+           currLink.classList.add('link-color');
+        } else {
+            currLink.classList.remove('link-color');
+        }    
+    } 
+}
+
+
+window.document.addEventListener('scroll', onScroll);
+
